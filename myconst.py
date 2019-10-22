2f445b2a8d220edb9347b6d10012184d722599e9
KOT244 <56309278+KOT244@users.noreply.github.com>
2019-10-22 17:44:16 +0200

    Add files via upload

---
+++ myconst.py
@@ -0,0 +1,12 @@
+GravConst = 6.67*10**(-11) 
+LightSpeed = 3*10**8
+Boltsman = 1.38*10**(-23) 
+Plank = 6.626*10**(-34)
+UskSvPad = 9.8 
+ElZar = 1.6*10**(-19) 
+AvagConst = 6*10**23 
+FaradConst = 9.65*10**4 
+UnGazConst = 8.31 
+RadBor = 0.53*10**(-10) 
+
+

592d771b5915f472a073b13eb867739e061cc2f5
KOT244 <56309278+KOT244@users.noreply.github.com>
2019-11-05 17:34:26 +0200

    Add files via upload

---
+++ допзадание1_ЛАБА5.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def grapgl(f=0.5):
+    x = np.ndarray
+    x = np.arange(-5,15,1)
+    F=1/f
+    for i in x:
+        if i<=5:
+            plt.plot([-3,0],[6,6])
+        else:
+                plt.plot([0,F],[6,0])
+    plt.show()
+c=grapgl()
+print(c)
+            
+    
